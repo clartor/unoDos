a461af3d2e688536d1649b49c7744b15de763a62
clartor <clara.tornblom@gmail.com>
2021-05-21 15:23:07 +0200

    funkar er

--- month.js
+++ month.js
@@ -1,90 +1,98 @@
-// import React, { useEffect, useState, Component  } from 'react';
-// import { View, Text, Modal, TouchableOpacity, TouchableHighlight, Dimensions } from 'react-native';
+import React, {Component} from 'react'
+import { View, Text, StyleSheet,TextInput, Button } from 'react-native'
+import { Formik } from 'formik'
 
-// const MonthYearPicker = (props) => {
-//     const month_data = [
-//         { key: 1, name: 'January' },
-//         { key: 2, name: 'February' },
-//         { key: 3, name: 'March' },
-//         { key: 4, name: 'April' },
-//         { key: 5, name: 'May' },
-//         { key: 6, name: 'June' },
-//         { key: 7, name: 'July' },
-//         { key: 8, name: 'August' },
-//         { key: 9, name: 'September' },
-//         { key: 10, name: 'October' },
-//         { key: 11, name: 'November' },
-//         { key: 12, name: 'December' },
-//     ]
+import birthMonth from './screens/form'
 
-//     const { width, height } = Dimensions.get('window')
+export default class Dragmonth extends Component {
 
-//     const [month, setMonth] = useState(month_data[new Date().getMonth()])
-//     // const [year, setYear] = useState(new Date().getFullYear())
-
-//     useEffect(() => {
-//         // props.onChangeYear(year)
-//         props.onChangeMonth(month_data[new Date().getMonth()])
-//     })
-
-//     return (
-//         <Modal
-//             animationType="slide"
-//             transparent={true}
-//             visible={props.isShow}
-//             onRequestClose={props.close}
-//         >
-//             <TouchableHighlight style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.3)' }} onPress={props.close}>
-//                 <View />
-//             </TouchableHighlight>
-//             <View style={{ flex: 1 / 2, backgroundColor: 'white' }} >
-//                 {/* <View style={styles.yearContainer}>
-//                     <TouchableOpacity onPress={() => {
-//                         setYear(year - 1)
-//                         props.onChangeYear(year - 1)
-//                     }}>
-//                         <Text>Prev</Text>
-//                     </TouchableOpacity>
-//                     <Text style={styles.yearLabel}>{year}</Text>
-//                     <TouchableOpacity onPress={() => {
-//                         setYear(year + 1)
-//                         props.onChangeYear(year + 1)
-//                     }}>
-//                         <Text>Next</Text>
-//                     </TouchableOpacity>
-//                 </View> */}
-//                 <View style={styles.monthContainer}>
-//                     {month_data.map((item, index) =>
-//                         <TouchableOpacity
-//                             key={index}
-//                             onPress={() => {
-//                                 setMonth(item)
-//                                 props.onChangeMonth(item)
-//                             }}
-//                             style={[styles.month, { width: (width / 3), backgroundColor: item.key == month.key ? "blue" : 'white' }]}>
-//                             <Text style={{ color: item.key == month.key ? 'white' : 'black' }}>{item.name}</Text>
-//                         </TouchableOpacity>
-//                     )}
-//                 </View>
-//             </View>
-//         </Modal>
-//     )
-// }
-
-// const styles = {
-//     monthContainer: {
-//         flexDirection: 'row',
-//         alignItems: 'center',
-//         flexWrap: 'wrap'
-//     },
-
-//     month: {
-//         height: 50, alignItems: 'center', justifyContent: 'center',
-//     },
-
-//     yearLabel: {
-//         fontWeight: 'bold', fontSize: 25
-//     },
-// }
-
-// export default MonthYearPicker
+  constructor(){
+    super();
+    this.state={
+      birthMonth : ''
+    }
+  }
+  
+  checkSwitch=(param)=>{
+    switch(param) {
+      case 'Januari':
+      case 'Juli':
+        this.One();
+        break;
+      case 'Februari':
+      case 'Augusti':
+        this.Two();
+        break;
+      case 'Mars':
+      case 'September':
+        this.Three();
+        break;
+        case 'April':
+        case 'Oktober':
+        this.Four();
+        break;
+      case 'Maj':
+      case 'November':
+        this.Five();
+        break;
+      case 'Juni':
+      case 'December':
+        this.Five();
+        break;
+      default:
+        Alert.alert("Månad finns inte");
+      }
+  }
+  One=()=>{
+    console.log("Miss");
+  }
+  Two=()=>{
+    console.log("Mrs");
+  }
+  Three=()=>{
+    console.log("Lady");
+  }
+  Four=()=>{
+    console.log("Dame");
+  }
+  Five=()=>{
+    console.log("Madame");
+  }
+  Six=()=>{
+    console.log("");
+   }
+   render() {
+  
+  return (
+    <Formik
+      initialValues={{birthMonth:'', password:''}}
+      onSubmit={(values) => console.log('submitted', values)}
+    >
+      {({handleChange, handleSubmit, values})=>(
+        <View>
+          <Text>Födelsemånad</Text>
+            <TextInput
+              style={styles.input}
+              value={values.birthMonth}
+              onChangeText={handleChange('birthMonth')}
+            />
+          <Text>Password</Text>
+          <TextInput
+            style={styles.input}
+            value={values.password}
+            onChangeText={handleChange('password')}
+            secureTextEntry={true}
+            />
+          <Button onPress={handleSubmit} title="Submit"/>
+        </View>
+      )}
+    </Formik>
+  )
+}}
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  }
+})
